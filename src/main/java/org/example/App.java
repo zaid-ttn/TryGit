4b16a5fd763339ff7303ea16363879d52460dcba
zaid-ttn <zaid.khan@tothenew.com>
2025-03-26 13:28:19 +0530

    hef

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {   int a=10;
+        int b=20;
+
+        System.out.println( "Hello World!" );
+    }
+}
